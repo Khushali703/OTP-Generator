a300116690c9a033270727126ed5ba1300ad6c63
Khushali703 <tankkhushali1@gmail.com>
2024-11-05 17:49:31 +0530

    random otp generator

---
+++ script.js
@@ -0,0 +1,25 @@
+
+let otpMsg = document.querySelector("#otpMsg");
+let btn = document.querySelector(".btn");
+let btn2 = document.querySelector(".btn2");
+ 
+
+//global variable
+let OTP = "";
+function generateOtp() {
+    OTP = Math.floor (Math.random() * 10 )+""+ //concat the each number into otp
+                Math.floor (Math.random() * 10 )+""+
+                Math.floor (Math.random() * 10 )+""+
+                 Math.floor (Math.random() * 10 )+""+
+                 Math.floor (Math.random() * 10 )+""+
+                 Math.floor (Math.random() * 10 );
+    
+    alert("Yur otp is generated successfully !"); // create the get otp alert
+    otpMsg.innerText = OTP;
+}
+btn.addEventListener('click', ()=>{
+ generateOtp();
+    let btn = document.querySelector(".btn").disabled = true;
+});
+
+
